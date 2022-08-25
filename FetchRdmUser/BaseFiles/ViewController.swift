c24bd3fe4ffb32d4df9542841629759fbd321c95
Saúl Pérez <saul.riot@gmail.com>
2022-08-24 23:33:58 -0500

    first commit

---
+++ FetchRdmUser/BaseFiles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchRdmUser
+//
+//  Created by Saúl Pérez on 24/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
