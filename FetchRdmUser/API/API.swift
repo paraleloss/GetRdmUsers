f47ffc99b1309796cb5eca864b8b36dc860af925
Saúl Pérez <saul.riot@gmail.com>
2022-08-25 02:40:54 -0500

    Adding splash screen

---
+++ FetchRdmUser/API/API.swift
@@ -0,0 +1,8 @@
+//
+//  API.swift
+//  FetchRdmUser
+//
+//  Created by Saúl Pérez on 25/08/22.
+//
+
+import Foundation
---
+++ FetchRdmUser/Extensions/UIViewController+SetBackground.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController+SetBackground.swift
+//  FetchRdmUser
+//
+//  Created by Saúl Pérez on 25/08/22.
+//
+
+import Foundation
